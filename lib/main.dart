acf19aa7a984ab3192cd48e0c48b481af9628c9f
GlebBrodskiy <h.brodskyi@newlinetechnologies.net>
2021-09-17 15:32:55 +0300

    Push from develop

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:filter_app/screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,8 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'ToDoшка',
+            style: GoogleFonts.lobster(color: Colors.white, fontSize: 40),
+          ),
+          backgroundColor: Colors.deepOrange,
+        ),
 
         body: Home(),
       ),
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-enum Importance {
-  yellow,
-  orange,
-  red,
-  darkBlue
+enum Importance { yellow, orange, red, darkBlue }
+
+extension ParseToString on Importance {
+  String toShortString() {
+    return this.toString().split('.').last;
+  }
 }
 
+Color textColor = Colors.orange;
 
-
+titleTextColor(Importance value) {
+  switch (value.toShortString()) {
+    case 'yellow':
+      textColor = Colors.yellow;
+      break;
+    case 'orange':
+      textColor = Colors.orange;
+      break;
+    case 'red':
+      textColor = Colors.red;
+      break;
+    case 'darkBlue':
+      textColor = Colors.blue.shade900;
+      break;
+  }
+}
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -21,76 +37,90 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   var _chosenValue;
 
-  _buildCircle(int index){
-
-  }
-
-
   List<Todo> myTodo = [
-    Todo('Eat breakfast', 'Eat some healthy food with coffee', '16.09.2021', Importance.yellow),
+    Todo('Eat breakfast', 'Eat some healthy food with coffee', '16.09.2021',
+        Importance.yellow),
     Todo('Play videogames', 'Complete one level', '16.09.2021', Importance.red),
-    Todo('Talk to Liana', 'Discuss the latest news', '16.09.2021', Importance.orange),
-    Todo('Talk to Dasha', 'Talk to Dasha about university problems', '16.09.2021', Importance.darkBlue),
+    Todo('Talk to Liana', 'Discuss the latest news', '16.09.2021',
+        Importance.orange),
+    Todo('Talk to Dasha', 'Talk to Dasha about university problems',
+        '16.09.2021', Importance.darkBlue),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Colors.deepPurple,
-          Colors.purple
-        ])
-      ),
+      color: Colors.white,
       child: SafeArea(
         child: Column(
           children: [
-            Text('ToDoшка', style: GoogleFonts.lobster(color: Colors.white, fontSize: 40),),
-            DropdownButton<Importance>(
-              dropdownColor: Colors.black,
-              style: TextStyle(color: Colors.white),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: DropdownButton<Importance>(
+                icon: Icon(
+                  Icons.filter_alt_outlined,
+                  color: Colors.deepOrange,
+                ),
+                isExpanded: true,
+                dropdownColor: Colors.black,
+                style: TextStyle(color: Colors.white),
                 iconEnabledColor: Colors.black,
                 value: _chosenValue,
-                items: Importance.values.map((Importance value){
-              return DropdownMenuItem<Importance>(
-                value: value,
-                child: Text(value.toString(), style: TextStyle(color: Colors.white),),
-              );
-            }).toList(),
-            onChanged: (Importance? value){
+                items: Importance.values.map((Importance value) {
+                  return DropdownMenuItem<Importance>(
+                    value: value,
+                    child: Text(
+                      value.toShortString(),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                }).toList(),
+                onChanged: (Importance? value) {
                   setState(() {
                     _chosenValue = value;
                   });
-            },),
-            ListView.builder(
-                itemCount: myTodo.length,
-                itemBuilder: (BuildContext context, int index){
-                return Container(
-                  width: MediaQuery.of(context).size.width*0.5,
-                  height: MediaQuery.of(context).size.height*0.5,
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(colors: [
-                      Colors.orange,
-                      Colors.purple
-                    ])
-                  ),
-                  child: Center(
-                    child: Column(
-                      children: [
-                        Text(myTodo[index].title),
-                        Text(myTodo[index].description),
-                        Row(
-                          children: [
-                            Text(myTodo[index].dateOfCreation),
-                            // Text(myTodo[index].importance),
-                          ],
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 90),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.5,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: myTodo.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      titleTextColor(myTodo[index].importance);
+                      return SizedBox(
+                        width: 300,
+                        height: 400,
+                        child: Card(
+                          child: Column(
+                            children: [
+                              Text(
+                                myTodo[index].title,
+                                style: TextStyle(color: textColor),
+                              ),
+                              Text(myTodo[index].description),
+                              Row(
+                                children: [
+                                  Text(myTodo[index].dateOfCreation),
+                                  Text(myTodo[index].dateOfCreation),
+                                  Text(myTodo[index].dateOfCreation),
+
+                                  // Text(myTodo[index].importance),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ],
-                    ),
-                  ),
-                );
-            })
+                      );
+                    }),
+              ),
+            )
           ],
         ),
       ),
@@ -98,14 +128,11 @@ class _HomeState extends State<Home> {
   }
 }
 
-class Todo{
+class Todo {
   final String title;
   final String description;
   final String dateOfCreation;
   final Importance importance;
 
   Todo(this.title, this.description, this.dateOfCreation, this.importance);
-
-
-
 }
